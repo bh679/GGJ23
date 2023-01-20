6515d2e3eb1cf252211f36dc1d14ee84fe6257b6
Brennan Hatton - MSImini <brennan@equalreality.com>
2023-01-20 21:41:53 +1100

    Text to speech thanks to MS

---
+++ Assets/PlayStoryWithVoice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BrennanHatton.Logging
+{
+	public class PlayStoryWithVoice : MonoBehaviour
+	{
+	    
+		public SpeechManager speech;
+		public StoryMaker story;
+		
+		int storyLength;
+		
+		void Start()
+		{
+			storyLength = story.inputResults.text.Length;
+		}
+	
+		// Update is called once per frame
+		void Update()
+		{
+			    
+			if(story.inputResults.text.Length != storyLength)
+			{
+				speech.SpeakWithSDKPlugin(story.inputResults.text);
+				// ```"+story.inputResults.text+"```" + "prompt:```"+story.inputPrompt.text+story.logger.output+"```");
+		    	
+				storyLength = story.inputResults.text.Length;
+			}
+		}
+	}
+}
