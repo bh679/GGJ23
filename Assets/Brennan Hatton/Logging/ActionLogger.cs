04ab38b6dbfdfc29bcab40e5a26e9686a65a98aa
Brennan Hatton MSI <bernnan@equalreality.com>
2023-01-20 14:21:26 +1100

    Logging demo

---
+++ Assets/Brennan Hatton/Logging/ActionLogger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrennanHatton.Logging
+{
+	[System.Serializable]
+	public class LogAction
+	{
+		public string who = "Player", did, what, with, when;
+		
+		public LogAction()
+		{
+			when = Time.time.ToString();
+		}
+		
+		public string GetString()
+		{
+			return who + " " + did + " " + what + " with " + with + " at " + when;
+		}
+	}
+
+	public class ActionLogger : MonoBehaviour
+	{
+		public List<LogAction> actions;
+		public string output;
+		
+		//Singlton
+		public static ActionLogger Instance { get; private set; }
+		private void Awake() 
+		{ 
+			// If there is an instance, and it's not me, delete myself.
+		    
+			if (Instance != null && Instance != this) 
+			{ 
+				Destroy(this); 
+			} 
+			else 
+			{ 
+				Instance = this; 
+			} 
+		}
+		
+		public void Add(LogAction log)
+		{
+			actions.Add(log);
+			output += log.GetString() + "\nThen ";
+		}
+		
+		public string GetString()
+		{
+			return output;/*
+			
+			string returnVal = "";
+			for(int i = 0; i < actions.Count; i++)
+				returnVal += actions[i].GetString() + "\n then ";
+			return returnVal;*/
+		}
+	}
+}
