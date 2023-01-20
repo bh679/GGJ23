601200a95b8c1ed531b17e0b26925f60f467a9d7
Brennan Hatton MSI <bernnan@equalreality.com>
2023-01-20 15:36:55 +1100

    Make a logging system and connecting it to GPT3 to tell stories based on user actions

---
+++ Assets/Brennan Hatton/Utilities/UI/NotificationDiscordLogger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BrennanHatton.Discord;
+
+namespace BrennanHatton.Notifications
+{
+
+	public class NotificationDiscordLogger : MonoBehaviour
+	{
+		public Notification notification;
+		bool sent = false;
+		
+		void Reset()
+		{
+			notification = this.GetComponent<Notification>();
+		}
+	    // Update is called once per frame
+	    void Update()
+	    {
+	        
+		    if(!sent && notification.isSetUp)
+		    {
+			    ClassDiscordConnection.Instance.SendMessage("Notification ``" + notification.title.text + "`` ``" + notification.body.text+"``");
+			    sent = true;
+		    }
+	    }
+	}
+
+}
