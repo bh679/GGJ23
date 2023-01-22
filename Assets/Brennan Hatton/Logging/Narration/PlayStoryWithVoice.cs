eb1267b6e674e196b54734d0a3caba610eca9d55
Brennan Hatton - MSImini <brennan@equalreality.com>
2023-01-22 17:15:31 +1100

    Realtime narrating

---
+++ Assets/Brennan Hatton/Logging/Narration/PlayStoryWithVoice.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BrennanHatton.Logging
+{
+	public class PlayStoryWithVoice : MonoBehaviour
+	{
+	    
+		public SpeechManager speech;
+		public StoryMaker story;
+		
+		int interactionsNumber;
+		bool executing = false;
+		
+		void Start()
+		{
+			interactionsNumber = story.GPTAPI.interactions.Count;
+			
+			executing = true;
+			
+			StartCoroutine(RunIntro());
+		}
+	
+		// Update is called once per frame
+		void Update()
+		{
+			
+			//if its not current making a story
+			if(!executing)
+			{
+				//if there are actions
+				if(story.logger.actions.Count > 0)
+				{
+					story.NarrateActions();
+					executing = true;
+				}else
+				{
+					story.AskQuestion();
+					executing = true;
+				}
+				
+			}
+			
+			//if not currently playing
+			if(speech.audioSource.isPlaying == false)
+			{
+				//if new story is avalible
+				if(executing && story.GPTAPI.interactions.Count != interactionsNumber)
+				{
+					speech.SpeakWithSDKPlugin(story.GPTAPI.interactions[story.GPTAPI.interactions.Count-1].generatedText);
+					// ```"+story.inputResults.text+"```" + "prompt:```"+story.inputPrompt.text+story.logger.output+"```");
+			    	
+					interactionsNumber = story.GPTAPI.interactions.Count;
+					executing = false;
+				}
+			}
+		}
+		
+		IEnumerator RunIntro()
+		{
+			yield return new WaitForSeconds(1f);
+			
+			story.Introduction();
+		}
+		
+	}
+}
---
+++ Assets/Brennan Hatton/Logging/Narration/StoryMaker.cs
@@ -0,0 +1,52 @@
+﻿// sample code by unitycoder.com
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityLibrary;
+
+namespace BrennanHatton.Logging
+{
+	public class StoryMaker : MonoBehaviour
+	{
+		public GPT3 GPTAPI;
+		public ActionLogger logger;
+		public TextAsset initalPrompt, followupPrompts, introduction, questions;
+		
+		
+		
+		/*public string GetStorySoFar()
+		{
+			string output = "";
+			
+			if(results.Count > 0)
+				output = "The narration so far has been: " + GetRecentResults(3) + followupPrompts.text;
+				
+			Debug.LogError(output);
+				
+			return output;
+		}*/
+		
+		public void Introduction()
+		{
+			GPTAPI.Execute(introduction.text);
+		}
+		
+		public void NarrateActions()
+		{
+			GPTAPI.Execute(initalPrompt.text +/* GetStorySoFar() + */"{"+logger.GetString()+"}");
+			logger.actions = new List<LogAction>();
+			logger.output = "";
+		}
+		
+		public void AskQuestion()
+		{
+			GPTAPI.Execute(questions.text);
+		}
+
+		
+
+	}
+}
