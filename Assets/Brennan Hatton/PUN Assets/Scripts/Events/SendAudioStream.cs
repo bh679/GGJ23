6ad1bb60df0d74d46ff4c1935196587344f736be
Brennan Hatton MSI <bernnan@equalreality.com>
2023-01-24 16:51:11 +1100

    Working on sending narration audio

--- Assets/Brennan Hatton/PUN Assets/Scripts/Events/SendAudioStream.cs
+++ Assets/Brennan Hatton/PUN Assets/Scripts/Events/SendAudioStream.cs
@@ -1,18 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO;
 
-public class SendAudioStream : MonoBehaviour
+namespace BrennanHatton.Networking.Events
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	
+	public class SendAudioStream : MonoBehaviourPunCallbacks
+	{
+		public SpeechManager speechManager;
+		
+		// If you have multiple custom events, it is recommended to define them in the used class
+		public const byte PlayerTakeDamage = 124;
+		
+		void Update()
+		{
+			//make it autosend
+		}
+		
+		public static void SendStreamEvent(int id, Stream audioStream)
+		{
+			Debug.Log("SendUpdateHealthEvent id:" + id + " audioStream:" + audioStream);
+			object[] content = new object[] { id, audioStream}; // Array contains the target position and the IDs of the selected units
+			RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All }; // You would have to set the Receivers to All in order to receive this event on the local client as well
+			PhotonNetwork.RaiseEvent(PlayerTakeDamage, content, raiseEventOptions, SendOptions.SendReliable);
+		}
+		
+	
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Brennan Hatton/PUN Assets/Scripts/Events/SendAudioStreamEventManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+namespace BrennanHatton.Networking.Events
+{
+	
+	public class SendAudioStreamEventManager : MonoBehaviourPunCallbacks
+	{
+		// If you have multiple custom events, it is recommended to define them in the used class
+		public const byte SteamEventCode = 124;
+		
+		
+		public static void SendUpdateHealthEvent(int id, Stream audioStream)
+		{
+			Debug.Log("SendUpdateHealthEvent id:" + id + " audioStream:" + audioStream);
+			object[] content = new object[] { id, audioStream }; // Array contains the target position and the IDs of the selected units
+			RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All }; // You would have to set the Receivers to All in order to receive this event on the local client as well
+			PhotonNetwork.RaiseEvent(SteamEventCode, content, raiseEventOptions, SendOptions.SendReliable);
+		}
+		
+	
+	}
+
+}
